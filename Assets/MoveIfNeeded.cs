3266ad1cec042a910ba8bd40508ccc53e63bb92e
AP Adrian Petrosanu <apetrosanu@airtouchmedia.com>
2017-04-26 15:37:46 +0300

    Initial project

---
+++ Assets/MoveIfNeeded.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveIfNeeded : MonoBehaviour {
+
+	[SerializeField] int LOOK_AWAY_TIME_TO_MOVE = 2;
+	[SerializeField] int speed;
+	GvrReticlePointerImpl pointer;
+	Camera camera;
+	bool isMoving = false;
+
+	// Use this for initialization
+	void Start () {
+		GvrReticlePointer pointer = GetComponentInChildren<GvrReticlePointer> ();
+		this.pointer = pointer.reticlePointerImpl;
+		camera = Camera.main;
+		speed = 40;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pointer.isExpanded == false && isMoving == false) {
+			Invoke ("Move", LOOK_AWAY_TIME_TO_MOVE);
+		} else {
+			CancelInvoke ();
+			isMoving = false;
+		}
+	}
+
+	void Move() {
+		isMoving = true;
+		transform.Translate (camera.transform.forward * Time.deltaTime * speed);
+	}
+}
---
+++ Assets/Scream.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scream : MonoBehaviour {
+
+	public GvrAudioSource scream;
+
+	public void OnTapped() {
+		scream.Play ();
+		Invoke ("Run", scream.clip.length);
+	}
+
+	public void onLookAway() {
+		scream.Stop ();
+		CancelInvoke ();
+	}
+
+	private void Run() {
+		gameObject.SetActive (false);
+	}
+}
